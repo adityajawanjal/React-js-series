856049f6cbcea68448ad062a33a21c53f33465a9
Aditya Jawanjal <adityajawanjal01@gmail.com>
2023-07-18 17:39:24 +0530

    done.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+export const BASE_URL = import.meta.env.BASE_URL || process.env.BASE_URL || `http://localhost:5000`;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>,
+  </React.StrictMode>,
 )
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import io from "socket.io-client";
 import ChatInfobar from "../components/ChatInfobar";
 import ChatSection from "../components/ChatSection";
 import ChatFooter from "../components/ChatFooter";
+import { BASE_URL } from "../main";
 
 let socket;
 const Chat = () => {
@@ -11,7 +12,7 @@ const Chat = () => {
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const endPoint = `http://localhost:5000`;
+  const endPoint = BASE_URL;
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -24,26 +25,30 @@ const Chat = () => {
     };
   }, [location.search]);
 
-  useEffect(()=>{  
-    socket.on('msg',(msg)=>{
-      setMessages([...messages , msg]);
-      setMessage('');
-    })
-  })
-  
-  const handleSendMessage = (e) =>{
+  useEffect(() => {
+    socket.on("msg", (msg) => {
+      setMessages([...messages, msg]);
+      setMessage("");
+    });
+  });
+
+  const handleSendMessage = (e) => {
     e.preventDefault();
-    if(message){
-      socket.emit('sendMessage',{name , room , message});
+    if (message) {
+      socket.emit("sendMessage", { name, room, message });
     }
-  }
+  };
 
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="flex flex-col w-11/12 md:w-6/12 border-2 border-black h-5/6">
-        <ChatInfobar room = {room} />
-        <ChatSection messages ={messages} name={name} />
-        <ChatFooter setMessage={setMessage} message={message} handleSendMessage={handleSendMessage} />
+        <ChatInfobar room={room} />
+        <ChatSection messages={messages} name={name} />
+        <ChatFooter
+          setMessage={setMessage}
+          message={message}
+          handleSendMessage={handleSendMessage}
+        />
       </div>
     </div>
   );
